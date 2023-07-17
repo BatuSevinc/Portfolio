2e1c255f5532e93b38a5497f2f001901e74dec4c
BatuSevinc <batusvnc1@gmail.com>
2023-07-17 17:29:06 +0300

    Update AboutContent.jsx

--- src/components/AboutContent/AboutContent.jsx
+++ src/components/AboutContent/AboutContent.jsx
@@ -12,8 +12,16 @@ const AboutContent = () => {
       <div className="left">
         <h1>Who Am I?</h1>
         <p>
-          I'm react front-end developer. I create responsive secure websites for
-          my clients.
+          I am Batuhan Sevinç, 24 years old and graduated from Civil
+          Engineering. I am eager to learn new things, value ethical principles,
+          and see myself as a confident, problem-solving and results-oriented
+          team player. I strive to be one of the best professionals in my field
+          by constantly improving myself. With my increasing interest in the
+          software industry, I decided to advance my career in this field. I
+          honed my skills by working on practical projects and constantly
+          researching to expand my knowledge. I don't like to be stuck with
+          limits, so I want to contribute to a company that will push the limits
+          and always move forward.
         </p>
         <Link to="/contact">
           <button className="btn">Contact</button>
